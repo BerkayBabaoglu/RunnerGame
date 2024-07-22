9441d29b7d35f0adcae78748595d74cc19072a84
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2024-07-22 19:59:19 +0300

    updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject obstacle;
+    public Transform _spawnPoint;
+    int score = 0;
+
+    public TextMeshProUGUI _scoreText;
+    public GameObject _playButton;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,34 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnOnstacles()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(0.5f, 2f);
+
+            yield return new WaitForSeconds(waitTime);
+
+            Instantiate(obstacle, _spawnPoint.position, Quaternion.identity);
+
+            
+        }
+    }
+
+    void ScoreUp()
+    {
+        score++;
+        _scoreText.text = score.ToString();
+    }
+
+    public void GameStart()
+    {
+        player.SetActive(true);
+        _playButton.SetActive(false);
+
+        StartCoroutine("SpawnOnstacles");
+        InvokeRepeating("ScoreUp",2f,1f);
     }
 }
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -23,4 +23,6 @@ public class Obstacle : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
+    
 }
